78873cb786008425772b9daf3841e98631c72ceb
rahul <rahulshrivardhanmarisetti@gmail.com>
2026-01-30 17:50:13 +0530

    commit

--- ttac_project/ttac_project/settings.py
+++ ttac_project/ttac_project/settings.py
@@ -80,14 +80,9 @@ WSGI_APPLICATION = 'ttac_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "ttac_ad",
-        "USER": "root",
-        "PASSWORD": "123456",
-        "HOST": "localhost",
-        "PORT": "3306",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
